01deff49bacd5dd21d2293d969292830d9b17166
Agnesa145 <agnesa.godeni@student.uni-pr.edu>
2026-05-09 19:45:28 +0200

    Update storage.py

--- storage.py
+++ storage.py
@@ -15,3 +15,7 @@ def create_empty_vault(master_password: str, path: Path = DEFAULT_VAULT_PATH) ->
     package = encrypt_vault(vault_data, master_password)
     save_package(package, path)
     return vault_data
+
+def load_vault(master_password: str, path: Path = DEFAULT_VAULT_PATH) -> Dict[str, Any]:
+    package = load_package(path)
+    return decrypt_vault(package, master_password)
